35e8a49d1d6187933aa6f2bcfeaf068b951c08fd
Arden <ardenisthebest@gmail.com>
2017-08-13 16:25:18 -0700

    Compleated first version

--- README.md
+++
@@ -1,6 +0,0 @@
-# Quickstart
-
-# Introduction
-
-Quickstart is a python script that is directed at implementing a starting point
-for C/C++ projects.
--- quickstart.py
+++ quickstart.py
@@ -70,7 +70,7 @@ class style:
             cyan = '\033[106m'
 
 
-__display_version__ = '0.1'
+__display_version__ = '0.2'
 
 
 def write(str, style_list=['']):
@@ -113,6 +113,12 @@ def boolean(x):
     return x.upper() in ('Y', 'YES')
 
 
+def is_type(x):
+    if x.upper() not in ('LIB', 'EXE'):
+        raise ValidationError("Pleasse enter either 'lib' or 'exe'.")
+    return x
+
+
 def do_prompt(d, name, description, default=None, validator=nonempty):
     if name in d:
         print(style.attr.bold + description + ': %s' % d[name] + style.normal)
@@ -146,25 +152,24 @@ accept a default value, if one is given in brackets).\n''')
     print(style.fg.cyan + '\n=====Basic Options=====\n' + style.normal)
     do_prompt(d, 'path', 'Root project path', '.', is_path)
     do_prompt(d, 'project', 'Project name')
+    do_prompt(d, 'type', 'Project Type (lib/exe)', 'lib', is_type)
+    do_prompt(d, 'git', 'Create git repo (y/n)', 'y', boolean)
     do_prompt(d, 'author', 'Author of project')
     do_prompt(d, 'version', 'Version of project', '1.0.0', allow_empty)
     print(style.fg.cyan + '\n=====Directory configuration=====\n' +
           style.normal)
-    do_prompt(d, 'source_dir', 'Source directory', 'src')
+    do_prompt(d, 'source_dir', 'Source directory', 'source')
+    do_prompt(d, 'include_dir', 'Include directory', 'include')
     do_prompt(d, 'build_dir', 'Build directory', 'build')
     do_prompt(d, 'ext_dir', 'External directory', 'external')
     do_prompt(d, 'test_dir', 'Test directory', 'test')
     do_prompt(d, 'doc_dir', 'Documentation directory', 'docs')
     print(style.fg.cyan + '\n=====Build configuration=====\n' + style.normal)
-    do_prompt(d, 'cmake', 'Generate Cmake (y/n)', 'n', boolean)
-    gnu_default='y'
-    if d['cmake'] is True:
-        gnu_default='n'
-    do_prompt(d, 'gnu_make', 'Generate GNU make (y/n)', gnu_default, boolean)
+    do_prompt(d, 'linker', 'Linked libraries', '', allow_empty)
     do_prompt(d, 'travis-ci', 'Create travis-ci integration (y/n)', 'y',
               boolean)
     do_prompt(d, 'codecov', 'Create codecov integration (y/n)', 'y', boolean)
-    do_prompt(d, 'sphinx', 'Create sphinx documenation (y/n)', 'y', boolean)
+    do_prompt(d, 'mkdocs', 'Create mkdocs documentation (y/n)', 'y', boolean)
     print(style.fg.cyan + '\n=====Description=====\n' + style.normal)
     do_prompt(d, 'description', 'Brief project description', None, allow_empty)
     for key in ('project', 'author', 'version'):
@@ -180,14 +185,17 @@ def mkdir(dir):
 def write_file(fpath, tpath, d):
     fpath = path.join(d['path'], fpath)
     tpath = path.join('templates/', tpath)
-    replace_list = [['project_name', d['project_str']],
-                    ['project_title', d['project_str'].title()],
-                    ['project_description', d['description']],
-                    ['project_source_dir', d['source_dir']],
-                    ['project_test_dir', d['test_dir']],
-                    ['project_build_dir', d['build_dir']],
-                    ['project_external_dir', d['ext_dir']],
-                    ['project_doc_dir', d['doc_dir']]]
+    replace_list = [['project_name', d['project_str']], [
+        'project_title', d['project_str'].title()
+    ], ['project_description',
+        d['description']], ['project_source_dir', d['source_dir']], [
+            'project_test_dir', d['test_dir']
+        ], ['project_build_dir',
+            d['build_dir']], ['project_external_dir',
+                              d['ext_dir']], ['project_doc_dir', d['doc_dir']],
+                    ['project_include_dir', d['include_dir']], [
+                        'project_link', d['linker'].replace(" ", " -l")
+                    ], ['project_type', d['type']]]
 
     with open(tpath, 'r') as file:
         filedata = file.read()
@@ -200,27 +208,42 @@ def write_file(fpath, tpath, d):
 
 
 def generate(d):
-    src_dir = path.join(d['path'], d['source_dir'])
+    d['linker'] = "-l" + d['linker']
+    source_dir = path.join(d['path'], d['source_dir'])
+    include_dir = path.join(d['path'], d['include_dir'])
     build_dir = path.join(d['path'], d['build_dir'])
     docs_dir = path.join(d['path'], d['doc_dir'])
     test_dir = path.join(d['path'], d['test_dir'])
     ext_dir = path.join(d['path'], d['ext_dir'])
-    mkdir(src_dir)
+    mkdir(source_dir)
+    mkdir(include_dir)
     mkdir(build_dir)
     mkdir(docs_dir)
     mkdir(test_dir)
     mkdir(ext_dir)
-    if d['cmake'] is True:
-        d['gnu_make'] = False
 
     write_file('README.md', 'README_t.md', d)
-    if d['cmake'] is True:
-        mkdir(path.join(d['path'], '.travis/cmake'))
-        write_file('.travis/cmake/CodeCoverage.cmake', 'coverage_t.cmake', d)
-        write_file('CMakeLists.txt', 'CMakeLists_t.txt', d)
-        write_file('.travis.yaml', 'travis_cmake_t.yaml', d)
-    elif d['gnu_make'] is True:
-        write_file('.travis.yaml', 'travis_make_t.yaml', d)
+    write_file('.gitignore', 'gitignore_t', d)
+    write_file('Makefile', 'Makefile_t', d)
+    write_file(path.join(d['source_dir'], 'Makefile'), 'source/Makefile_t', d)
+    write_file(path.join(d['source_dir'], 'main.cpp'), 'source/main_t.cpp', d)
+    write_file(path.join(d['ext_dir'], 'Makefile'), 'external/Makefile_t', d)
+    write_file(path.join(d['test_dir'], 'Makefile'), 'test/Makefile_t', d)
+    write_file(
+        path.join(d['test_dir'], 'test_tmp.cpp'), 'test/test_tmp_t.cpp', d)
+    if d['travis-ci'] is True:
+        write_file('.travis.yaml', 'travis_t.yml', d)
+    if d['codecov'] is True:
+        write_file('.codecov.yml', 'codecov_t.yml', d)
+    if d['mkdocs'] is True:
+        write_file('mkdocs.yml', 'mkdocs_t.yml', d)
+        write_file(path.join(d['doc_dir'], 'index.md'), 'docs/index_t.md', d)
+    if d['git'] is True:
+        os.system('git init ' + d['path'])
+        os.system(
+            'cd ' + d['path'] +
+            ' && git submodule add https://github.com/google/googletest.git ' +
+            path.join(d['ext_dir'], 'googletest'))
 
 
 def main():
@@ -237,6 +260,15 @@ def main():
         dest='project',
         help='project name')
     group.add_option(
+        '--type', metavar='TYPE', dest='type', help='project type(lib/exe)')
+    group.add_option(
+        '-g',
+        '--git',
+        action='store_true',
+        dest='git',
+        default=False,
+        help='Creates git repository')
+    group.add_option(
         '-a', '--author', metavar='AUTHOR', dest='author', help='author names')
     group.add_option(
         '-v', metavar='VERSION', dest='version', help='version of project')
@@ -247,6 +279,12 @@ def main():
         metavar='PATH',
         dest='source_dir',
         help='path to source directory')
+    group.add_option(
+        '-i',
+        '--include',
+        metavar='PATH',
+        dest='include_dir',
+        help='path to include directory')
     group.add_option(
         '-b',
         '--build',
@@ -273,19 +311,7 @@ def main():
         help='path to externals directory')
     group = parser.add_option_group('Build configuration')
     group.add_option(
-        '-c',
-        '--cmake',
-        action='store_true',
-        dest='cmake',
-        default=False,
-        help='Create cmake configuration (Will override --make)')
-    group.add_option(
-        '-m',
-        '--make',
-        action='store_true',
-        dest='gnu_make',
-        default=False,
-        help='Create GNU make configuration')
+        '-l', '--link', metavar='LINK', dest='linker', help='linked libraries')
     group.add_option(
         '--travis',
         action='store_true',
@@ -299,11 +325,11 @@ def main():
         default=False,
         help='Create CodeCov integration')
     group.add_option(
-        '--sphinx',
+        '--mkdocs',
         action='store_true',
-        dest='sphinx',
+        dest='mkdocs',
         default=False,
-        help='Creates sphinx documentation')
+        help='Creates mkdocs documentation')
 
     try:
         opts, args = parser.parse_args(sys.argv[1:])
